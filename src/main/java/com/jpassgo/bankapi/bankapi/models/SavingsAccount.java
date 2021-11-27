aa68a4347824c7a4327dc1aa335a5a3753a562cd
jpassgo <jeffpascoe9@gmail.com>
2021-11-26 21:07:20 -0600

    Updating class

--- src/main/java/com/jpassgo/bankapi/bankapi/models/SavingsAccount.java
+++ src/main/java/com/jpassgo/bankapi/bankapi/models/SavingsAccount.java
@@ -2,6 +2,9 @@ package com.jpassgo.bankapi.bankapi.models;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 public class SavingsAccount extends Account {
+    private BigDecimal interestAccruedAmmount;
 }
