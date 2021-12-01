283d73946c658f0992403d0b74a5b32ad7cb2024
jpassgo <jeffpascoe9@gmail.com>
2021-11-30 19:57:33 -0600

    We can now get an account of a specified type now.

--- src/main/java/com/jpassgo/bankapi/bankapi/controllers/AccountsController.java
+++ src/main/java/com/jpassgo/bankapi/bankapi/controllers/AccountsController.java
@@ -1,24 +1,40 @@
 package com.jpassgo.bankapi.bankapi.controllers;
 
+import com.jpassgo.bankapi.bankapi.enums.AccountType;
 import com.jpassgo.bankapi.bankapi.models.Account;
+import com.jpassgo.bankapi.bankapi.models.CheckingAccount;
+import com.jpassgo.bankapi.bankapi.models.SavingsAccount;
 import com.jpassgo.bankapi.bankapi.services.AccountsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+import static com.jpassgo.bankapi.bankapi.enums.AccountType.*;
+
+@RestController("/accounts")
 @RequiredArgsConstructor
 public class AccountsController {
 
   private final AccountsService accountsService;
 
   @PostMapping
-  public Integer createAccount(Account account) {
-    return accountsService.createCheckingAccount();
+  public String createAccount(String userId, Account account) {
+    if(account.getAccountType().equals(CHECKING_ACCOUNT)) {
+      return accountsService.createCheckingAccount(userId, (CheckingAccount) account);
+    } else {
+      return accountsService.createSavingsAccount(userId,(SavingsAccount) account);
+    }
   }
 
   @GetMapping
-  public Account getAccount(int number) {
-    return null;
+  public ResponseEntity<Account> getAccount(String number, AccountType accountType) {
+    var maybeAccount = accountsService.getAccount(number, accountType);
+    if(maybeAccount.isPresent()) {
+      return new ResponseEntity(maybeAccount.get(), HttpStatus.OK);
+    } else {
+      return new ResponseEntity(HttpStatus.BAD_REQUEST);
+    }
   }
 
   @PutMapping
